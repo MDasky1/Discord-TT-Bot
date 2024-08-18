108730d657855d74f96d63e50e7d93acc5d807dd
mdask <mdasky1@yahoo.com>
2024-08-18 04:06:19 -0400

    Added a field to determine if the user is entered in the contest or not.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ bot: Bot = commands.Bot(command_prefix='!', intents=intents)
 
 '''
 Attempt to load a contest winners file. File is a Python dictionary in the following format:
-    User.ID : [User.Name], Date and Time of last win (1-1-2000 is set if never won)
+    User.ID : [User.Name], Entered, Date and Time of last win (1-1-2000 is set if never won)
 '''
 try:
     with open('contestwinners.pickle', 'rb') as handle:
@@ -33,7 +33,7 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
     for member in bot.get_all_members():
         if not contestwinners.get(member.id, 0):
-            contestwinners[member.id] = [member.name, datetime(2000, 1, 1,0,0,0)]
+            contestwinners[member.id] = [member.name, False, datetime(2000, 1, 1,0,0,0)]
     print(contestwinners)
     with open('contestwinners.pickle', 'wb') as handle:
         pickle.dump(contestwinners, handle)
